2ca0c06497330688ab1fdfe71775b672da767409
iannathan-k <ianmtv@gmail.com>
2026-04-14 21:48:57 -0700

    full proposal

---
+++ wireframe.cpp
@@ -0,0 +1,21 @@
+// Include all headers
+
+#include <iostream>
+#include <map>
+#include <array>
+#include <list>
+#include <random>
+
+using namespace std;
+
+// Define function to simulate reactor over time
+// Parameters: map of 
+
+int main() {
+
+    // Open file
+
+    // Close file
+
+    return 0;
+}
