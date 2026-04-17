4a627822029229b9f2cd4107a9e9ba3881211d80
iannathan-k <ianmtv@gmail.com>
2026-04-17 10:02:07 -0700

    comparmentalization

--- wireframe.cpp
+++ wireframe.cpp
@@ -18,12 +18,19 @@ const int MIN_MELTDOWN_TEMP = 1200;
 const int MAX_MELTDOWN_POWER = 1000000;
 const int MIN_MELTDOWN_POWER = 500000;
 
+const string SYSTEM_1 = "CORE";
+const string SYSTEM_2 = "COOLING";
+const string SYSTEM_3 = "WASTE";
+
 // Define function to simulate reactor over 1 time period
     // Parameters: map of components
 void step(map<string, array<list<string>, 3>>&);
 // Define function to print reactor at time
     // Parameters: map of components
 void output(const map<string, array<list<string>, 3>>&);
+
+void activateElement(const list<string>&, string);
+void deactivateElement(const list<string>&);
 
 // Define main
 int main() {
@@ -45,9 +52,15 @@ int main() {
         // Raise error
     // Close file
 
-    reactor["core"][0].push_back("ROD_1");
+    reactor["core"][0].push_back("BOILER_1");
     reactor["core"][1].push_back("250MW");
     reactor["core"][2].push_back("367C");
+    reactor["cooling"][0].push_back("ROD_1");
+    reactor["cooling"][1].push_back("250MW");
+    reactor["cooling"][2].push_back("367C");
+    reactor["waste"][0].push_back("TANK_1");
+    reactor["waste"][1].push_back("250MW");
+    reactor["waste"][2].push_back("367C");
 
     output(reactor);
 
@@ -67,56 +80,54 @@ void step(map<string, array<list<string>, 3>>& reactor) {
         // Randomly decide to start a meltdown
         // Randomly decide to active/deactive any components
 
-    for (auto& system : reactor.second) {
+    for (auto& system : reactor) {
+        cout << "~~~~~~~~ " << system.first << " ~~~~~~~~" << endl;
+         // 80% probability of fluctuations
+        int prob = rand() % MAX + 1; // Generates numbers 0 to 99 inclusive
+        if (prob < 80) {
+            cout << "Normal Fluctuations Occuring..." << endl;
+            // Fluctuate Powers
+            for (string& power : system.second[1]) {
+                power = to_string(rand() % (MAX_POWER - MIN_POWER + 1) + MIN_POWER) + "MW";
+            }
 
-    }
-
-    // 80% probability of fluctuations
-    int prob = rand() % MAX + 1; // Generates numbers 0 to 99 inclusive
-    if (prob < 80) {
-        cout << "Normal Fluctuations Occuring..." << endl;
-        // Fluctuate Powers
-        for (string& power : reactor["core"][1]) {
-            power = to_string(rand() % (MAX_POWER - MIN_POWER + 1) + MIN_POWER) + "MW";
+            // Fluctuare Temps
+            for (string& temp : system.second[2]) {
+                temp = to_string(rand() % (MAX_TEMP - MIN_TEMP + 1) + MIN_TEMP) + "C";
+            }
         }
 
-        // Fluctuare Temps
-        for (string& temp : reactor["core"][2]) {
-            temp = to_string(rand() % (MAX_TEMP - MIN_TEMP + 1) + MIN_TEMP) + "C";
-        }
-    }
-
-    // Deactivate an element
-    prob = rand() % MAX + 1;
-    if (prob < 15 && reactor["core"][0].size() > 0) {
-        cout << "Deactivating an element..." << endl;
-        reactor["core"][0].pop_back();
-        reactor["core"][1].pop_back();
-        reactor["core"][2].pop_back();
-    }
-
-    // Active an element
-    prob = rand() % MAX + 1;
-    if (prob < 20) {
-        cout << "Activating an element..." << endl;
-        reactor["core"][0].push_back("ROD_" + to_string(reactor["core"][0].size() + 1));
-        reactor["core"][1].push_back(to_string(rand() % (MAX_POWER - MIN_POWER + 1) + MIN_POWER) + "MW");
-        reactor["core"][2].push_back(to_string(rand() % (MAX_TEMP - MIN_TEMP + 1) + MIN_TEMP) + "C");
-    }
-
-    // Temperature surge!!!
-    prob = rand() % MAX + 1;
-    if (prob < 5) {
-        cout << "!! EMERGENCY !! Power Surge Occuring!!!!" << endl;
-        for (string& power : reactor["core"][1]) {
-            power = to_string(rand() % (MAX_MELTDOWN_POWER - MIN_MELTDOWN_POWER + 1) + MIN_MELTDOWN_POWER) + "MW";
+        // Deactivate an element
+        prob = rand() % MAX + 1;
+        if (prob < 15 && system.second[0].size() > 0) {
+            cout << "Deactivating an element..." << endl;
+            system.second[0].pop_back();
+            system.second[1].pop_back();
+            system.second[2].pop_back();
         }
 
-        for (string& temp : reactor["core"][2]) {
-            temp = to_string(rand() % (MAX_MELTDOWN_TEMP - MIN_MELTDOWN_TEMP + 1) + MIN_MELTDOWN_TEMP) + "C";
+        // Active an element
+        prob = rand() % MAX + 1;
+        if (prob < 20) {
+            cout << "Activating an element..." << endl;
+            system.second[0].push_back("ROD_" + to_string(system.second[0].size() + 1));
+            system.second[1].push_back(to_string(rand() % (MAX_POWER - MIN_POWER + 1) + MIN_POWER) + "MW");
+            system.second[2].push_back(to_string(rand() % (MAX_TEMP - MIN_TEMP + 1) + MIN_TEMP) + "C");
+        }
+
+        // Temperature surge!!!
+        prob = rand() % MAX + 1;
+        if (prob < 5) {
+            cout << "!! EMERGENCY !! Power Surge Occuring!!!!" << endl;
+            for (string& power : system.second[1]) {
+                power = to_string(rand() % (MAX_MELTDOWN_POWER - MIN_MELTDOWN_POWER + 1) + MIN_MELTDOWN_POWER) + "MW";
+            }
+
+            for (string& temp : system.second[2]) {
+                temp = to_string(rand() % (MAX_MELTDOWN_TEMP - MIN_MELTDOWN_TEMP + 1) + MIN_MELTDOWN_TEMP) + "C";
+            }
         }
     }
-    
 }
 
 void output(const map<string, array<list<string>, 3>>& reactor) {
@@ -124,6 +135,7 @@ void output(const map<string, array<list<string>, 3>>& reactor) {
         // Output active elements
         // Output electricity draws
         // Output temperatures
+    cout << "~~~~~~~ CURRENT ~~~~~~~~" << endl;
 
     for (auto system : reactor) {
         cout << "Active Components: "; 
@@ -144,4 +156,19 @@ void output(const map<string, array<list<string>, 3>>& reactor) {
         }
         cout << endl;
     }
+}
+
+void activateElement(const list<string>& system, string system_name) {
+    string prefix = "BOILER_";
+    if (system_name == SYSTEM_2) {
+        prefix = "ROD_";
+    } else if (system_name == SYSTEM_3) {
+        prefix = "TANK_";
+    } else {
+
+    }
+
+    system.push_back(to_string(rand() % (MAX_POWER - MIN_POWER + 1) + MIN_POWER) + "MW");
+    system.push_back(to_string(rand() % (MAX_TEMP - MIN_TEMP + 1) + MIN_TEMP) + "C");
+    
 }
