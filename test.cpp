d841d5cffa4a0c0cc288fb88fc4b797375d55c17
iannathan-k <ianmtv@gmail.com>
2026-04-17 15:20:43 -0700

    druver unit test

--- test.cpp
+++ test.cpp
@@ -6,6 +6,11 @@ using namespace std;
 
 // COMSC-210 | Lab 29 | Ian Kusmiantoro
 
+void activateElement(array<list<string>, 3>&, string);
+void deactivateElement(array<list<string>, 3>&);
+void fluctuate(array<list<string>, 3>&);
+void surge(array<list<string>, 3>&);
+
 bool testFluctuate(const array<list<string>, 3>& reactor) {
     array<list<string>, 3> copy = reactor;
 
@@ -13,6 +18,7 @@ bool testFluctuate(const array<list<string>, 3>& reactor) {
 
     if (copy[0] != reactor[0]) {
         // Fluctuate should not affect the elements!
+        cout << "Elements should not be affected!" << endl;
         return false;
     }
 
@@ -22,16 +28,26 @@ bool testFluctuate(const array<list<string>, 3>& reactor) {
         }
     }
 
+    cout << "All power and temperatures are the same!" << endl;
     return false;
 }
 
 bool testActivate(const array<list<string>, 3>& reactor) {
     array<list<string>, 3> copy = reactor;
 
-    activateElement(reactor, "CORE");
+    activateElement(copy, "CORE");
 
     for (int i = 0; i < 3; i++) {
-        if (copy[i].size() != reactor[i].size() + 1) {
+        if (copy[i].size() > reactor[i].size() + 1) {
+            cout << "More than 1 element added!" << endl;
+            return false; // Did not only add 1 element!
+        }
+        if (copy[i].size() == reactor[i].size() + 1) {
+            cout << "No elements added!" << endl;
+            return false; // Did not only add 1 element!
+        }
+        if (copy[i].size() < reactor[i].size()) {
+            cout << "Elements were removed!" << endl;
             return false; // Did not only add 1 element!
         }
     }
@@ -42,10 +58,19 @@ bool testActivate(const array<list<string>, 3>& reactor) {
 bool testDeactive(const array<list<string>, 3>& reactor) {
     array<list<string>, 3> copy = reactor;
 
-    deactivateElement(reactor);
+    deactivateElement(copy);
 
     for (int i = 0; i < 3; i++) {
-        if (copy[i].size() != reactor[i].size() - 1) {
+        if (copy[i].size() < reactor[i].size() - 1) {
+            cout << "More than 1 element removed!" << endl;
+            return false; // Did not only remove 1 element!
+        }
+        if (copy[i].size() > reactor[i].size()) {
+            cout << "Elements were added!" << endl;
+            return false; // Did not only remove 1 element!
+        }
+        if (copy[i].size() == reactor[i].size()) {
+            cout << "No elements were removed!" << endl;
             return false; // Did not only remove 1 element!
         }
     }
@@ -59,15 +84,44 @@ bool testMeltdown(const array<list<string>, 3>& reactor) {
     surge(copy);
 
     if (copy[0] != reactor[0]) {
-        // Fluctuate should not affect the elements!
+        // Meltdown should not affect the elements!
+        cout << "Elements should not be affected!" << endl;
         return false;
     }
 
     for (int i = 1; i < 3; i++) {
         if (copy[i] != reactor[i]) {
-            return true; // We found the array to be different, so we can it did successfully fluctuate
+            return true; // We found the array to be different, so we can it did successfully meltdown
         }
     }
 
+    cout << "Power and temps are the same!" << endl;
     return false;
+}
+
+int main() {
+    array<list<string>, 3> reactor;
+
+    if (testActivate(reactor)) {
+        cout << "Activation test passed." << endl;
+    } else {
+        cout << "Activation test failed!" << endl;
+    }
+    if (testFluctuate(reactor)) {
+        cout << "Fluctuation test passed" << endl;
+    } else {
+        cout << "Fluctuation test failed!" << endl;
+    }
+    if (testMeltdown(reactor)) {
+        cout << "Meltdown test passed" << endl;
+    } else {
+        cout << "Meltdown test failed!" << endl;
+    }
+    if (testDeactive(reactor)) {
+        cout << "Deactivation test passed" << endl;
+    } else {
+        cout << "Deactivation test failed!" << endl;
+    }
+
+    return 0;
 }
